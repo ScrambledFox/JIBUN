2f99c9828daf4e950d90856ce9b85902da579dab
ScrambledFox <github@jorislodewijks.nl>
2023-10-18 00:44:52 +0200

    fix: works for real now

--- src/components/threejs/cubes.tsx
+++ src/components/threejs/cubes.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import { Canvas, extend } from "@react-three/fiber";
 import { Physics, useBox, usePlane } from "@react-three/cannon";
+import { Box3 } from "three";
 
 interface CubesProps {
   cubeAmount: number;
@@ -11,8 +12,18 @@ interface CubesProps {
 const Cubes = ({ cubeAmount }: CubesProps) => {
   return (
     <div className="absolute h-full w-full left-0 top-0 -z-50">
-      <Canvas shadows camera={{ position: [0, 2, 7] }}>
-        <ambientLight intensity={1} />
+      <Canvas dpr={[1, 2]} shadows camera={{ position: [0, 0, 7], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+
+        <spotLight
+          intensity={255}
+          angle={0.5}
+          position={[25, 25, 15]}
+          decay={1}
+          penumbra={1}
+          color={"#fff"}
+          castShadow
+        />
 
         <Physics>
           {Array.from({ length: cubeAmount }).map((_, i) => (
@@ -45,17 +56,18 @@ const Floor = (props: any) => {
   );
 };
 
-const Cube = () => {
+const Cube = (props: any) => {
   const [ref] = useBox(() => ({
-    position: [Math.random() * 10 - 5, 25, Math.random() * 10 - 10],
-    rotation: [Math.random() * 10, Math.random() * 10, Math.random() * 10],
+    position: [Math.random() * 20 - 10, 25, Math.random() * 10 - 20],
+    rotation: [Math.random() * 360, Math.random() * 360, Math.random() * 360],
     mass: 1,
+    ...props,
   }));
 
   return (
-    <mesh ref={ref} receiveShadow castShadow>
-      <boxGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="white" />
+    <mesh ref={ref} {...props} receiveShadow>
+      <boxGeometry />
+      <meshStandardMaterial color="orange" />
     </mesh>
   );
 };
