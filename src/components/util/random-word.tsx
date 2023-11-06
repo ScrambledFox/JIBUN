66950bc0bfa943323fbb6101d858a2011e7d1d20
ScrambledFox <github@jorislodewijks.nl>
2023-11-06 22:19:38 +0100

    fix: Client-rendering and state for beer time

--- src/components/util/random-word.tsx
+++ src/components/util/random-word.tsx
@@ -1,9 +1,20 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 interface RandomWordProps {
   words: string[];
 }
 
 const RandomWord = ({ words }: RandomWordProps) => {
-  const randomWord = words[Math.floor(Math.random() * words.length)];
+  const [randomWord, setRandomWord] = useState<string>();
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    setRandomWord(words[randomIndex]);
+
+    document.title = "TIJD VOOR BIER 🍻";
+  }, [words]);
 
   return (
     <div className="flex flex-row justify-center items-center">
